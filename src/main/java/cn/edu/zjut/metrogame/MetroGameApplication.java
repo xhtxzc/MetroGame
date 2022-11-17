1f931c4318138378ec3be87fb53d04e92f5067e6
docker <xhtxzc@hotmail.com>
2022-11-17 18:25:12 +0800

    first

---
+++ src/main/java/cn/edu/zjut/metrogame/MetroGameApplication.java
@@ -0,0 +1,11 @@
+package cn.edu.zjut.metrogame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetroGameApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MetroGameApplication.class, args);
+    }
+}
---
+++ src/test/java/cn/edu/zjut/metrogame/MetroGameApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.edu.zjut.metrogame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetroGameApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
